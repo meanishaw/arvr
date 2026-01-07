b9aae3ebdb0818479faa13939f184bde361b80c8
meanishaw <127970045+meanishaw@users.noreply.github.com>
2026-01-07 10:58:36 +0530

    Implement FirstPersonController for player movement

---
+++ 3.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    float xRotation = 0f;
+
+    void Update()
+    {
+        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
